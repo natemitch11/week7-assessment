d365519fad334923558506b05c9536845a78d6e3
natemitch11 <nate.mitch11@gmail.com>
2021-10-01 16:09:02 -0600

    fixed indentation

--- part-3/functions.js
+++ part-3/functions.js
@@ -1,57 +1,54 @@
 const perf = require('execution-time')();
 
 class AddCache {
-    constructor(){
-        this.cache = {}
-    }
+  constructor() {
+    this.cache = {};
+  }
 
-    sumZero(array){
-        perf.start()
-        array.sort()
-        for (let i = 0; i < array.length; i++){
-          if (array[i]< 0 && array.includes(array[i]*-1)){
-            let results = perf.stop().preciseWords
-            console.log('Sum Zero Runtime:', results)
-            return true
-          }
-          else if (array[i]>= 0) {
-            let results = perf.stop().preciseWords
-            console.log('Sum Zero Runtime:', results)
-            return false
-            
-          }
-        }
-        
+  sumZero(array) {
+    perf.start();
+    array.sort();
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < 0 && array.includes(array[i] * -1)) {
+        let results = perf.stop().preciseWords;
+        console.log("Sum Zero Runtime:", results);
+        return true;
+      } else if (array[i] >= 0) {
+        let results = perf.stop().preciseWords;
+        console.log("Sum Zero Runtime:", results);
+        return false;
+      }
     }
+  }
 
-    countLetters(str) {
-        this.cache = {}
-        str = str.toLowerCase().replace(/([^a-z])+/g,'')
-        for (let i = 0; i < str.length; i++) {
-          if (this.cache.hasOwnProperty(str[i])) {
-            this.cache[str[i]]++
-          } else {
-            this.cache[str[i]] = 0
-          }
-        }
-        return this.cache
+  countLetters(str) {
+    this.cache = {};
+    str = str.toLowerCase().replace(/([^a-z])+/g, "");
+    for (let i = 0; i < str.length; i++) {
+      if (this.cache.hasOwnProperty(str[i])) {
+        this.cache[str[i]]++;
+      } else {
+        this.cache[str[i]] = 0;
+      }
     }
+    return this.cache;
+  }
 
-    uniqueChar(string){
-        perf.start()
-        let values = Object.values(this.countLetters(string)).sort().pop()
-        let results = perf.stop().preciseWords
-        console.log('Unique Character Runtime:', results)
-        return (values === 0 ? true : false)
-    }
+  uniqueChar(string) {
+    perf.start();
+    let values = Object.values(this.countLetters(string)).sort().pop();
+    let results = perf.stop().preciseWords;
+    console.log("Unique Character Runtime:", results);
+    return values === 0 ? true : false;
+  }
 
-    isPangram(string){
-        perf.start()
-        let values = Object.keys(this.countLetters(string))
-        let results = perf.stop().preciseWords
-        console.log('Is a Pangram Runtime:', results)
-        return (values.length === 26 ? true : false)
-    }
+  isPangram(string) {
+    perf.start();
+    let values = Object.keys(this.countLetters(string));
+    let results = perf.stop().preciseWords;
+    console.log("Is a Pangram Runtime:", results);
+    return values.length === 26 ? true : false;
+  }
 }
 
 let testArr1 = [1,2,3,4,5,6,-7,-8,-5]
